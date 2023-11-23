139fb05a45cde503bf848115580e875907c202a2
Luiz Vicente <luizcv89@gmail.com>
2023-11-23 15:35:26 -0300

    Add wasm files

---
+++ game/pkg/game_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main_js(): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke1_mut__h078a91c95b6f85c4(a: number, b: number, c: number): void;
+export function wasm_bindgen__convert__closures__invoke1_mut__hb77c457a62c77831(a: number, b: number, c: number): void;
+export function wasm_bindgen__convert__closures__invoke0_mut__he7f6eab1fdbeec14(a: number, b: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
